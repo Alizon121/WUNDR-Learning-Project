dd9ac9a797f3aa99cbe41279a616c5a6ab2fc842
joshuamaxey <joshua.maxey.leija@icloud.com>
2025-07-11 13:34:13 -0500

    Wrote and tested 'Create Child' route

--- backend/routers/child.py
+++ backend/routers/child.py
@@ -1,14 +1,44 @@
-from fastapi import APIRouter, status, Depends
+from fastapi import APIRouter, status, Depends, HTTPException
 from db.prisma_client import db
 from typing import Annotated
-from models.user_models import User
-from .auth.login import get_current_active_user
+from models.user_models import User, Child, ChildCreate
+from .auth.login import get_current_user
 
 router = APIRouter()
 
-@router.get("/", status_code=status.HTTP_200_OK)
+# ! Create Child
+@router.post("", status_code=status.HTTP_201_CREATED)
+async def create_child(
+    child_data: ChildCreate,
+    current_user: Annotated[User, Depends(get_current_user)]
+
+):
+    try:
+        created_child = await db.children.create(
+            data={
+                "firstName": child_data.firstName,
+                "lastName": child_data.lastName,
+                "homeschool": child_data.homeschool,
+                "age": child_data.age,
+                "parentIDs": [current_user.id], # Add the urrent user's ID to parentIDs
+                "activityIDs": [] # Create activityIDs array so we can easily add to it later
+            },
+            include={
+                "parents": True, # Include the current user in the child's 'parents' array
+            }
+        )
+
+    except Exception as e:
+        raise HTTPException(
+            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+            detail=f"Failed to create child: {str(e)}"
+        )
+
+    return {"child": created_child, "message": "Child added successfully"}
+
+@router.get("/childrenOfCurrentUser", status_code=status.HTTP_200_OK)
 async def get_children(
-    current_user: Annotated[User, Depends(get_current_active_user)]
+    current_user: Annotated[User, Depends(get_current_user)]
 ):
     children = await db.children.find_many(
         where={

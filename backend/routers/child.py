91f1433da8f85f945373a46744ab6be98346dc95
joshuamaxey <joshua.maxey.leija@icloud.com>
2025-07-11 15:04:18 -0500

    Wrote and tested 'Update Child' route

--- backend/routers/child.py
+++ backend/routers/child.py
@@ -1,15 +1,18 @@
 from fastapi import APIRouter, status, Depends, HTTPException
 from db.prisma_client import db
 from typing import Annotated
-from models.user_models import User, ChildCreate
+from models.user_models import User, ChildCreate, ChildUpdate
 from .auth.login import get_current_user
 
+
 router = APIRouter()
+
 
 # ! Create Child
 @router.post("", status_code=status.HTTP_201_CREATED)
 async def create_child(
-    child_data: ChildCreate, # apply Pydantic model for data validation
+    # apply Pydantic model for data validation
+    child_data: ChildCreate,
     current_user: Annotated[User, Depends(get_current_user)]
 
 ):
@@ -64,6 +67,7 @@ async def get_children(
     )
     return children
 
+
 # ! Get Child by ID
 @router.get("/{child_id}", status_code=status.HTTP_200_OK)
 async def get_child_by_id(
@@ -95,3 +99,46 @@ async def get_child_by_id(
         )
 
     return child
+
+
+# ! Update Child
+@router.patch("/{child_id}", status_code=status.HTTP_200_OK)
+async def update_child(
+    child_id: str,
+    # use ChildUpdate pydantic model for data validation
+    update_data: ChildUpdate,
+    current_user: Annotated[User, Depends(get_current_user)]
+):
+    # Fetch the child
+    child = await db.children.find_unique(
+        where={"id": child_id}
+    )
+
+    if not child:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail=f"Child with ID {child_id} not found."
+        )
+
+    # Make sure the current user is a parent of the child
+    if current_user.id not in child.parentIDs:
+        raise HTTPException(
+            status_code=status.HTTP_403_FORBIDDEN,
+            detail="Access denied: You are not a parent of this child."
+        )
+
+    # Make the update
+    updated_child = await db.children.update(
+        where={"id": child.id},
+        # exclude any fields that were not included in the payload
+        data=update_data.model_dump(exclude_unset=True),
+        include={
+            "parents": True,
+            "activities": True
+        }
+    )
+
+    return {
+        "child": updated_child,
+        "message": "Child updated successfully"
+    }

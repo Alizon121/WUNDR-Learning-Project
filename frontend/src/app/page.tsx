f6b77f54a533ca3d5e4971b93b8ae264c1271a5f
AnastasiiaMuzh <anastasiia.muzhzhavlev@outlook.com>
2025-07-30 09:55:18 -0600

    Refactor: moved components folder into src

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,9 +1,9 @@
 "use client";
 
-import HeroSection from "./components/landing/HeroSection";
-import Story_Mission from "./components/landing/Story_Mission";
-import ImpactStats from "./components/landing/ImpactNumbers";
-import OurPrograms from "./components/landing/OurPrograms"
+import HeroSection from "../components/landing/HeroSection";
+import Story_Mission from "../components/landing/Story_Mission";
+import ImpactStats from "../components/landing/ImpactNumbers";
+import OurPrograms from "../components/landing/OurPrograms"
 
 export default function LandingPage() {
   return (

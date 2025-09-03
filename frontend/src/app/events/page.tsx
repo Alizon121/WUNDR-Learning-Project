2f47c90ac79ae01d240659ef96c79fb2f22c244f
joshuamaxey <joshua.maxey.leija@gmail.com>
2025-09-03 14:20:15 -0500

    Updated Events Page to include and conditionally render ADD EVENT button for Admins

--- frontend/src/app/events/page.tsx
+++ frontend/src/app/events/page.tsx
@@ -1,91 +1,93 @@
-"use client"
+"use client";
 
 import { useEffect, useState } from "react";
 import ActivityBlock from "@/components/eventsPage/ActivityBlock";
 import { makeApiRequest } from "../../../utils/api";
 
 interface Event {
-    id: string;
-    name: string;
-    description: string;
-    date: string;
-    image: string;
-    participants: number;
+  id: string;
+  name: string;
+  description: string;
+  date: string;
+  image: string;
+  participants: number;
 }
 
 interface Activity {
-    id: string;
-    name: string;
-    description: string;
-    events: Event[]
+  id: string;
+  name: string;
+  description: string;
+  events: Event[];
 }
 
 interface GroupedEvents {
-    activity: string;
-    events: Event[]
+  activity: string;
+  events: Event[];
 }
 
 export default function EventsPage() {
-    const [groupedEvents, setGroupedEvents] = useState<GroupedEvents[]>([]);
-    const [loading, setLoading] = useState(true);
-    const [isAdmin, setIsAdmin] = useState(false);
+  const [groupedEvents, setGroupedEvents] = useState<GroupedEvents[]>([]);
+  const [loading, setLoading] = useState(true);
+  const [isAdmin, setIsAdmin] = useState(false);
 
-    useEffect(() => {
-        const user = localStorage.getItem("user");
-        if (user) {
-            const userObj = JSON.parse(user);
-            setIsAdmin(userObj.role === "admin");
-        }
-    })
+  useEffect(() => {
+    const user = localStorage.getItem("user");
+    if (user) {
+      const userObj = JSON.parse(user);
+      setIsAdmin(userObj.role === "admin");
+    }
+  });
 
-    useEffect(() => {
+  useEffect(() => {
+    const fetchActivitiesWithEvents = async () => {
+      try {
+        const { activities } = await makeApiRequest<{ activities: Activity[] }>(
+          "http://localhost:8000/activity/with-events"
+        );
 
-        const fetchActivitiesWithEvents = async () => {
+        const formatted: GroupedEvents[] = activities.map((activity) => ({
+          activity: activity.name,
+          events: activity.events,
+        }));
 
-            try {
+        setGroupedEvents(formatted);
+      } catch (err) {
+        console.error("Failed to fetch activities with events:", err);
+      } finally {
+        setLoading(false);
+      }
+    };
 
-                const { activities } = await makeApiRequest<{ activities: Activity[] }>(
-                    "http://localhost:8000/activity/with-events"
-                );
+    fetchActivitiesWithEvents();
+  }, []);
 
-                const formatted: GroupedEvents[] = activities.map((activity) => ({
-                    activity: activity.name,
-                    events: activity.events
-                }));
+  return (
+    <main className="px-6 py-8 max-w-5xl md:max-w-7xl mx-auto bg-wonderbg min-h-screen">
+      <div className="text-center mb-12">
+        <h1 className="text-4xl font-bold mb-4 text-green-800">
+          Upcoming Events
+        </h1>
+        <p className="text-green-700 max-w-2xl mx-auto text-lg">
+          Connect with other homeschooling families through hands-on
+          experiences, outdoor adventures, and educational opportunities.
+        </p>
+        {isAdmin && (
+          <button className="mt-2 bg-green-700 text-white px-10 py-2 rounded text-sm font-medium hover:bg-green-800 transition-colors">
+            <strong>Add Event</strong>
+          </button>
+        )}
+      </div>
 
-                setGroupedEvents(formatted);
-
-            } catch (err) {
-                console.error("Failed to fetch activities with events:", err);
-            } finally {
-                setLoading(false)
-            }
-        };
-
-        fetchActivitiesWithEvents();
-    }, []);
-
-    return (
-        <main className="px-6 py-8 max-w-5xl md:max-w-7xl mx-auto bg-wonderbg min-h-screen">
-
-            <div className="text-center mb-12">
-                <h1 className="text-4xl font-bold mb-4 text-green-800">Upcoming Events</h1>
-                <p className="text-green-700 max-w-2xl mx-auto text-lg">
-                    Connect with other homeschooling families through hands-on experiences, outdoor adventures, and educational opportunities.
-                </p>
-            </div>
-
-            {groupedEvents.map(({ activity, events }) => (
-
-                // <div>
-                <ActivityBlock
-                    key={activity}
-                    activityName={activity}
-                    events={events}
-                    isAdmin={isAdmin}
-                />
-                // </div>
-            ))}
-        </main>
-    )
+      {groupedEvents.map(({ activity, events }) => (
+        // <div>
+        <ActivityBlock
+          key={activity}
+          activityName={activity}
+          events={events}
+          isAdmin={isAdmin}
+        />
+        // </div>
+      ))}
+    </main>
+  );
 }

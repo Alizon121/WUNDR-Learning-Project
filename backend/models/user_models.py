287230a755be587ab621b07c731b118b2ee445d9
Andrew <andrew.lizon12@gmail.com>
2025-09-10 11:31:07 -0700

    Create PATCH router for updating volunteer and volunteer opportunity:

--- backend/models/user_models.py
+++ backend/models/user_models.py
@@ -114,7 +114,6 @@ class VolunteerCreate(BaseModel):
   bio: Optional[str] = Field(None, min_length=5, max_length=500, description="Volunteer bio")
   photoConsent: bool = Field(..., description="Must consent to photo usage")
   backgroundCheckConsent: bool = Field(..., description="Must consent to background check")
-  # userId: str = Field(..., description="Associated user ID")
 
   @field_validator("cities", "timesAvail", "skills", mode="before")
   def clean_lists(cls, v):

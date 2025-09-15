29d00605daddf31083f0c10682cf43ce7776a19c
joshuamaxey <joshua.maxey.leija@gmail.com>
2025-09-15 18:05:53 -0500

    Updated AuthGuard.tsx to check if token is present, then to check expiration, then delete if expired.

--- frontend/src/components/auth/AuthGuard.tsx
+++ frontend/src/components/auth/AuthGuard.tsx
@@ -1,21 +1,23 @@
-"use client"
+"use client";
 
 import { useRouter } from "next/navigation";
 import { useEffect } from "react";
 import { isTokenExpired } from "../../../utils/auth";
 
 export default function AuthGuard() {
-    const router = useRouter();
-    
-    useEffect(() => {
+  const router = useRouter();
+
+  useEffect(() => {
     const token = localStorage.getItem("token");
 
-    if (!token || isTokenExpired(token)) {
-        localStorage.removeItem("token");
-        localStorage.removeItem("user");
-        router.push("/")
-    }
-    }, [])
+    // if (!token) return;
 
-    return null;
+    if (token && isTokenExpired(token)) {
+      localStorage.removeItem("token");
+      localStorage.removeItem("user");
+      router.push("/");
+    }
+  }, []);
+
+  return null;
 }

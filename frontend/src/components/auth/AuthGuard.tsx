7e176f248371d6a6aaefdf27b06af6c05895ae5e
joshuamaxey <joshua.maxey.leija@gmail.com>
2025-09-15 17:52:49 -0500

    Wrote and implemented AuthGuard.tsx in layout to automatically logout/redirect if token is expiried

---
+++ frontend/src/components/auth/AuthGuard.tsx
@@ -0,0 +1,21 @@
+"use client"
+
+import { useRouter } from "next/navigation";
+import { useEffect } from "react";
+import { isTokenExpired } from "../../../utils/auth";
+
+export default function AuthGuard() {
+    const router = useRouter();
+    
+    useEffect(() => {
+    const token = localStorage.getItem("token");
+
+    if (!token || isTokenExpired(token)) {
+        localStorage.removeItem("token");
+        localStorage.removeItem("user");
+        router.push("/")
+    }
+    }, [])
+
+    return null;
+}
